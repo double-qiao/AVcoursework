4bcb97456291b900997b9d3c2ca9cf3024062c70
zpole <zhi951206@gmail.com>
2019-02-26 16:08:46 +0000

    first commit

---
+++ reading_points.m
@@ -0,0 +1,22 @@
+%% Load the training data 
+office = load('office1.mat');
+office = office.pcl_train;
+%% Uncomment to load the test file
+% office = load('office2.mat');
+% office = office.pcl_test;
+%%
+for i = 1:length(office) % Reading the 40 point-clouds
+i	  
+    rgb = office{i}.Color; % Extracting the colour data
+    point = office{i}.Location; % Extracting the xyz data
+    z_threshold = find(point(:,3) > 3.5 );
+    rgb(z_threshold,:) = 255;
+    point(z_threshold,:) = NaN;
+    pc = pointCloud(point, 'Color', rgb); % Creating a point-cloud variable
+    
+    figure(1)
+    pcshow(pc)
+    figure(2)
+    imag2d(rgb) % Shows the 2D images
+    pause
+end
